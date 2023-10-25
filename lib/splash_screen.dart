d113bb02f830125003486bc272c8f80c16fd1b04
ak-shf <pachaaakarsh@gmail.com>
2023-10-25 22:21:57 +0530

    qwe

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,6 +10,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
