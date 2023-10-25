45e7e15389a44ed7a62e6c9b15832b6cf9f4ab67
ak-shf <pachaaakarsh@gmail.com>
2023-10-25 22:12:57 +0530

    aws

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -8,10 +8,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  
   @override
   Widget build(BuildContext context) {
-   
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
